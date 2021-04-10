645921e07b8cca66c08269f85216e0eb9c70e9f1
kimon986 <kimonmono986@gmail.com>
2021-04-10 12:12:57 -0700

    finished asmt 4

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,15 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { createForms } from 'react-redux-form';
-import { Dishes } from './dishes';
-import { Promotions } from './promotions';
-import { Leaders } from './leaders';
-import { Comments } from './comments';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import {InitialFeedback} from "./forms";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createForms } from "react-redux-form";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { InitialFeedback } from "./forms";
+import { Dishes } from "./dishes";
+import { Comments } from "./comments";
+import { Promotions } from "./promotions";
+import { Leaders } from "./leaders";
 
 export const ConfigureStore = () => {
-    const store = createStore (
+    const store = createStore(
         combineReducers({
             dishes: Dishes,
             comments: Comments,
@@ -23,4 +23,4 @@ export const ConfigureStore = () => {
     );
 
     return store;
-}
+};
